f874d58e60742e4baa2c874242eafb3ce2864598
Sophonie Bouye <sophoniebouye@gmail.com>
2025-07-16 12:49:21 +0000

    feat: usre service

---
+++ src/services/user.service.ts
@@ -0,0 +1,74 @@
+import { Prisma } from "../generated/prisma";
+import { UserResponse } from "../types/user.types";
+import prisma from "../utils/prisma-client";
+
+const create = async (data: Prisma.UserCreateInput): Promise<UserResponse> => {
+  return await prisma.user.create({
+    data,
+    select: {
+      id: true,
+      name: true,
+      username: true,
+      email: true,
+      createdAt: true,
+    },
+  });
+};
+
+const findById = async (
+  id: Prisma.UserWhereUniqueInput["id"],
+  withPassword: boolean = false
+): Promise<UserResponse> => {
+  return await prisma.user.findUniqueOrThrow({
+    where: { id },
+    select: {
+      id: true,
+      name: true,
+      username: true,
+      password: withPassword,
+      email: true,
+      createdAt: true,
+    },
+  });
+};
+
+const findByEmail = async (
+  email: Prisma.UserWhereUniqueInput["email"],
+  withPassword: boolean = false
+): Promise<UserResponse> => {
+  return await prisma.user.findUniqueOrThrow({
+    where: { email },
+    select: {
+      id: true,
+      name: true,
+      username: true,
+      password: withPassword,
+      email: true,
+      createdAt: true,
+    },
+  });
+};
+
+const update = async (
+  id: Prisma.UserWhereUniqueInput["id"],
+  data: Prisma.UserUpdateInput
+): Promise<UserResponse> => {
+  return await prisma.user.update({
+    where: { id },
+    data,
+    select: {
+      id: true,
+      name: true,
+      username: true,
+      email: true,
+      createdAt: true,
+    },
+  });
+};
+
+export default {
+  create,
+  update,
+  findById,
+  findByEmail,
+};
